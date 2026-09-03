a8abe6b472e0d5aae1067359401ff7712a59eea3
Jesse Torres <torresjesse18@gmail.com>
2026-09-03 18:25:38 -0500

    feat: api handler for prodicts-service

--- cmd/products-service/main.go
+++ cmd/products-service/main.go
@@ -1,11 +1,28 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
+	"strings"
 )
 
 func main() {
+	http.HandleFunc("/products/", func(w http.ResponseWriter, r *http.Request) {
+		id, err := strconv.Atoi(strings.TrimPrefix(r.URL.Path, "/products/"))
+		if err != nil {
+			http.Error(w, "invalid product id", http.StatusBadRequest)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(map[string]interface{}{
+			"id":      id,
+			"name":    fmt.Sprintf("Product %d", id),
+			"service": "products",
+		})
+	})
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Products Service")
 	})
