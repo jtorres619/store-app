2b8816a4fd643ffe4ee27b4255119d8a1d4bea21
Jesse Torres <torresjesse18@gmail.com>
2026-09-03 18:22:59 -0500

    feat: api handler for orders-services

--- cmd/orders-service/main.go
+++ cmd/orders-service/main.go
@@ -1,11 +1,28 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
+	"strings"
 )
 
 func main() {
+	http.HandleFunc("/orders/", func(w http.ResponseWriter, r *http.Request) {
+		id, err := strconv.Atoi(strings.TrimPrefix(r.URL.Path, "/orders/"))
+		if err != nil {
+			http.Error(w, "invalid order id", http.StatusBadRequest)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(map[string]interface{}{
+			"id":      id,
+			"name":    fmt.Sprintf("Order %d", id),
+			"service": "orders",
+		})
+	})
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Orders Service")
 	})
